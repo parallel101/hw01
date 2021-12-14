558f890fb07fd6fcc247002e854f5deb671dc49c
zhangyazhou <885150974@qq.com>
2021-12-14 23:21:14 +0800

    add hw01 soulation

---
+++ stbiw/stb_image_write.cpp
@@ -0,0 +1,10 @@
+#define STB_IMAGE_WRITE_IMPLEMENTATION 
+// 定义这个宏是防止多次编译（老师，上课讲过），
+// 类似于
+/**
+ * 1. #pragma once
+ * 2. #ifndef XXX
+ *    #define XXX
+ *    #endif
+ */
+#include "stb_image_write.h"
