01cc0a7f261d56c66e4892978c5bd076e6681a13
pwqbot <ding00000804@gmail.com>
2021-12-15 22:16:26 +0800

    Finish homework qwq

--- rainbow.cpp
+++ rainbow.cpp
@@ -3,13 +3,13 @@
 #include <vector>
 
 void test_rainbow() {
-    std::vector<char> buf(512 * 512 * 3);
-    for (int j = 0; j < 512; j++) {
-        for (int i = 0; i < 512; i++) {
-            buf[(j * 512 + i) * 3 + 0] = i / 2;
-            buf[(j * 512 + i) * 3 + 1] = j / 2;
-            buf[(j * 512 + i) * 3 + 2] = 0;
+        std::vector<char> buf(512 * 512 * 3);
+        for (int j = 0; j < 512; j++) {
+                for (int i = 0; i < 512; i++) {
+                        buf[(j * 512 + i) * 3 + 0] = i / 2;
+                        buf[(j * 512 + i) * 3 + 1] = j / 2;
+                        buf[(j * 512 + i) * 3 + 2] = 0;
+                }
         }
-    }
-    stbi_write_png("rainbow.png", 512, 512, 3, buf.data(), 0);
+        stbi_write_png("rainbow.png", 512, 512, 3, buf.data(), 0);
 }
