730bbd9a28bb758a4a8595224d432b7b15a09918
MengXiao92 <csumengxiao@163.com>
2021-12-15 11:44:12 +0800

    hw01 by mengx

---
+++ stbiw/stbiw.cpp
@@ -0,0 +1,9 @@
+/*************************************************************************
+	> File Name: stbiw.cpp
+	> Author: mengxiao
+	> Mail: csumengxiao@163.com 
+	> Created Time: Wed 15 Dec 2021 11:41:09 AM CST
+ ************************************************************************/
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
