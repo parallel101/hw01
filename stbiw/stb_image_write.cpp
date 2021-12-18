3a4417004349c038b948726c41d1236a1592535c
pengguanjun <pengguanjun@users.noreply.github.com>
2021-12-18 23:08:08 +0800

    vs2019 attempt,bad result.

---
+++ stbiw/stb_image_write.cpp
@@ -0,0 +1,11 @@
+
+#ifndef STBI_WRITE_NO_STDIO
+#define STBI_WRITE_NO_STDIO
+#endif
+
+#ifndef STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#endif
+
+//#include <stb_image_write.h>
+#include "stb_image_write.h"
