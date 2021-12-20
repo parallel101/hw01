c819ee4bba35d3246eae7fc0b0980f156dde2614
hit-liang <281558509@qq.com>
2021-12-20 12:08:49 +0800

    hw01

---
+++ CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/mandel.cpp.o"
+  "CMakeFiles/main.dir/rainbow.cpp.o"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ stbiw/CMakeFiles/stbiw.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stbiw.dir/stb_image_write.cpp.o"
+  "libstbiw.a"
+  "libstbiw.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stbiw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
