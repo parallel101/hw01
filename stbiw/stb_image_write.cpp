7c2081c5474e9ae4c18fea2ce436540f76e8f8d5
Ted Liu <mfpczs@outlook.com>
2022-01-11 20:51:13 +0800

    homework - finish

---
+++ stbiw/stb_image_write.cpp
@@ -0,0 +1,5 @@
+// why we should #define STB.. at this cpp file
+// can we just use the .h file? why cmake doesn't spport?
+// author is Changhui Liu
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
